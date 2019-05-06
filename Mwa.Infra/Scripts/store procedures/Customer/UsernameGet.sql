7fa4a05a92b7f4b5e3dc11b8d4621649a0ff89fc
Gabbs1110 <gabb8091@gmail.com>
2019-05-05 22:54:38 -0300

    alteração na query de consulta do customer

--- Mwa.Infra/Scripts/store procedures/Customer/UsernameGet.sql
+++ Mwa.Infra/Scripts/store procedures/Customer/UsernameGet.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE UsernameGet
         @UserName VARCHAR(40) 
 AS
 	
-SELECT [Id], CONCAT([FirstName], ' ', [LastName]) AS [Name], [BirthDate], [Document],
+SELECT [Id], CONCAT([FirstName], ' ', [LastName]) AS [Name], [BirthDate], [Email], [Document],
         [UserName], [Password], [Active]
 
 FROM [Customer]
